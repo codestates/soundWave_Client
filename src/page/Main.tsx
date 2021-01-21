2453bd5216e75f78d3a954f0d8037b072501d041
youme <mrsimplelife@naver.com>
2021-01-21 11:21:43 +0900

    [client]Merge main-animation

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,8 +1,10 @@
 import Footer from "../components/main-footer/Footer";
+import raintest from "../components/main-animation/rainEffect";
 
 function Main() {
   return (
     <>
+      {raintest()}
       <Footer />
     </>
   );
