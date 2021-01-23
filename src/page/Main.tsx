ac4740c9f70fd549e9226b09f3cd88ab5616afae
jeongho3786 <jeongho3786@gmail.com>
2021-01-23 21:12:06 +0900

    [client]setting street lamp

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,11 +1,15 @@
 import Footer from "../components/main-footer/Footer";
 import raintest from "../components/main-animation/rainEffect";
+import streetLamp from "../components/main-animation/streetLamp";
+
 function Main() {
   return (
     <>
-      {raintest()}
+      {/* {raintest()} */}
+      {/* {streetLamp()} */}
       <Footer />
     </>
   );
 }
+
 export default Main;
