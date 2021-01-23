dbee380473d0bd591b503ad95fecd2472c13376a
jeongho3786 <jeongho3786@gmail.com>
2021-01-24 06:33:11 +0900

    [client]setting car, road

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,12 +1,16 @@
 import Footer from "../components/main-footer/Footer";
 import raintest from "../components/main-animation/rainEffect";
 import streetLamp from "../components/main-animation/streetLamp";
+import bounceCar from "../components/main-animation/bounceCar";
+import mainRoad from "../components/main-animation/mainRoad";
 
 function Main() {
   return (
     <>
-      {/* {raintest()} */}
+      {raintest()}
+      {mainRoad()}
       {/* {streetLamp()} */}
+      {/* {bounceCar()}; */}
       <Footer />
     </>
   );
