ecb44e869877fa73d75d9fd4c15ae3c52440e422
jeongho3786 <jeongho3786@gmail.com>
2021-01-26 10:48:08 +0900

    [client]add moving road and lamp

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,29 +1,27 @@
 import Footer from "../components/main-footer/Footer";
-import raintest from "../components/main-animation/rainEffect";
 import { useSelector } from "react-redux";
 import { RootState } from "../reducer";
 import Modal from "../components/main-search/Modal";
 import styled from "styled-components";
 import SearchForm from "../components/main-search/SearchForm";
-import streetLamp from "../components/main-animation/streetLamp";
-import bounceCar from "../components/main-animation/bounceCar";
 import mainRoad from "../components/main-animation/mainRoad";
 
 const Container = styled.div`
   height: 100%;
   position: relative;
   background: linear-gradient(to bottom, #202020, #111119);
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
 `;
+
 function Main() {
   const isSearchOpen = useSelector(
     (state: RootState) => state.musicSearch.isSearchOpen
   );
   return (
     <Container>
-      {/* {raintest()} */}
       {mainRoad()}
-      {/* {streetLamp()}
-      {bounceCar()}; */}
       <Footer />
       {isSearchOpen && (
         <Modal>
