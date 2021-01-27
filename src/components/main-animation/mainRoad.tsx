7b3d38c230a10763bb59637560f45874c070e3cd
jeongho3786 <jeongho3786@gmail.com>
2021-01-28 03:01:05 +0900

    [client]add night effect

--- src/components/main-animation/mainRoad.tsx
+++ src/components/main-animation/mainRoad.tsx
@@ -2,12 +2,14 @@ import streetLamp from "./streetLamp";
 import bounceCar from "./bounceCar";
 import raintest from "./rainEffect";
 import waveEffect from "./waveEffect";
-import "./mainRoad.css";
+import nightEffect from "./nightEffect";
 import longRoadimage from "../../images/road_long.png";
+import "./mainRoad.css";
 
 function mainRoad() {
   return (
     <div className="mainRoad">
+      {nightEffect()}
       <img id="road" src={longRoadimage} alt="" />
       {bounceCar()}
       {streetLamp()}
